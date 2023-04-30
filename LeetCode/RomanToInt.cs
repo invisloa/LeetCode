ab9abe3cbc4d1180e97b8f6a15cfbe3523587ef2
invisloa <91217120+invisloa@users.noreply.github.com>
2023-04-30 23:26:00 +0200

    Add project files.

---
+++ LeetCode/RomanToInt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	internal class RomanToInt
+	{
+
+		Dictionary<string, int> romanToIntDictionary = new Dictionary<string, int>()
+		{
+		{ "I",1 },
+		{ "V",5 },
+		{ "X",10 },
+		{ "L",50 },
+		{ "C",100 },
+		{ "D",500 },
+		{ "M",1000 }
+		};
+		int RomanToIntTransform(string s)
+		{
+			int resoultValue = 0;
+			int prevValue = 0;
+
+			for (int i = s.Length - 1; i >= 0; i--)
+			{
+				int currValue = romanToIntDictionary[s[i].ToString()];
+				if (currValue < prevValue)
+				{
+					resoultValue -= currValue;
+				}
+				else
+				{
+					resoultValue += currValue;
+				}
+				prevValue = currValue;
+			}
+			char b = s[0];
+			return resoultValue;
+		}
+	}
+}
