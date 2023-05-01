03642879b91bdd5d3208f1756e09148d1f77a87a
invisloa <91217120+invisloa@users.noreply.github.com>
2023-05-01 14:54:29 +0200

    Add Two Numbers working with carry

---
+++ LeetCode/AddListNodeNumbers.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+	public class AddListNodeNumbers
+	{
+		ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+		{
+			int carry = 0;
+			ListNode head = new ListNode(0);
+			ListNode curr = head;
+
+			while (l1 != null || l2 != null || carry != 0)
+			{
+				int sum = carry;
+
+				if (l1 != null)
+				{
+					sum += l1.val;
+					l1 = l1.next;
+				}
+
+				if (l2 != null)
+				{
+					sum += l2.val;
+					l2 = l2.next;
+				}
+
+				carry = sum / 10;
+				sum = sum % 10;
+
+				curr.next = new ListNode(sum);
+				curr = curr.next;
+			}
+
+			return head.next;
+		}
+
+		public class ListNode
+		{
+			public int val;
+			public ListNode next;
+			public ListNode(int val = 0, ListNode next = null)
+			{
+				this.val = val;
+				this.next = next;
+			}
+		}
+
+	}
+}
